21ed6074b1478aa5c015d0b3be4d1e664ed4a8db
StupidBrooom <ytbridger@gmail.com>
2023-04-18 12:56:10 +0200

    first commit

---
+++ server/models/product.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    category: { type: String, required: true},
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("product", productSchema);
